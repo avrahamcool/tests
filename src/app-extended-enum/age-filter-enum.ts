d7203dad0eff54feb3c204766c58fb8ce4cd6290
Avraham Essoudry <essoudry@gmail.com>
2023-04-17 19:49:29 +0300

    better extended enum

--- src/app-extended-enum/age-filter-enum.ts
+++ src/app-extended-enum/age-filter-enum.ts
@@ -1,23 +1,28 @@
 export class AgeFilterEnum
 {
+	//#region Implementation
 	private static _uniqueId = 0;
 	private static _entries: AgeFilterEnum[] = [];
-
-	static NoFilter: Readonly<AgeFilterEnum> = new AgeFilterEnum("הכל");
-	static Adult: Readonly<AgeFilterEnum> = new AgeFilterEnum("מבוגרים");
-	static Children: Readonly<AgeFilterEnum> = new AgeFilterEnum("ילדים");
-
-	public id: Readonly<number>;
-
-	private constructor(public displayName: Readonly<string>)
-	{
-		this.id = ++AgeFilterEnum._uniqueId;
-
-		AgeFilterEnum._entries.push(this);
-	}
 
 	public static [Symbol.iterator](): IterableIterator<AgeFilterEnum>
 	{
 		return AgeFilterEnum._entries[Symbol.iterator]();
+	}
+
+	//#endregion Implementation
+
+	static readonly NoFilter = new AgeFilterEnum("הכל");
+	static readonly Adult = new AgeFilterEnum("מבוגרים");
+	static readonly Children = new AgeFilterEnum("ילדים");
+
+	public readonly id: number;
+	public readonly displayName: string;
+
+	private constructor(displayName: string)
+	{
+		this.id = AgeFilterEnum._uniqueId++;
+		this.displayName = displayName;
+
+		AgeFilterEnum._entries.push(this);
 	}
 }
--- src/app-extended-enum/app.ts
+++ src/app-extended-enum/app.ts
@@ -1,3 +1,11 @@
+import { AgeFilterEnum } from "./age-filter-enum";
 export class App
 {
+	constructor()
+	{
+		for(const i of AgeFilterEnum)
+		{
+			console.log(i);
+		}
+	}
 }
